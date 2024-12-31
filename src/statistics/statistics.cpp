e83c4de2e824db248c71fd0e3a205f607aeae465
sunsmart96 <sunsmart96@gmail.com>
2024-12-31 21:40:33 +0800

    添加读取xlnt和fmt两个外部库

--- src/statistics/statistics.cpp
+++ src/statistics/statistics.cpp
@@ -1,6 +1,28 @@
-import std;
 import statistics;  // 导入模块  
 
+#include <fmt/core.h>
+#include <xlnt/xlnt.hpp>
+
+// ANSI 转义序列  
+constexpr const char* RED = "\033[31m";
+constexpr const char* GREEN = "\033[32m";
+constexpr const char* YELLOW = "\033[33m";
+constexpr const char* BLUE = "\033[34m";
+constexpr const char* RESET = "\033[0m";  // 重置颜色  
+
 void hello() {
-    std::cout << "hello23" << std::endl;
+    fmt::print("{}Hello, {}!\n", RED, "World");
+    fmt::print("{}This is a green message.\n", GREEN);
+    fmt::print("{}This is a yellow message.\n", YELLOW);
+    fmt::print("{}This is a blue message.\n", BLUE);
+    fmt::print("{}This is a message with no color.\n", RESET);
+
+    xlnt::workbook wb;
+    xlnt::worksheet ws = wb.active_sheet();
+    ws.cell("A1").value(5);
+    ws.cell("B2").value("string data");
+    ws.cell("C3").formula("=RAND()");
+    ws.merge_cells("C3:C4");
+    ws.freeze_panes("B2");
+    wb.save("example.xlsx");
 }
